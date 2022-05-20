b291690902387d485ce5e975ed6a227d82ec7ea5
Adrian Campoverde Len <addriank12@outlook.com>
2022-05-20 08:25:46 -0500

    Main

--- src/main/java/com/mycompany/ejercicio03_02/Main.java
+++ src/main/java/com/mycompany/ejercicio03_02/Main.java
@@ -4,6 +4,8 @@
  */
 package com.mycompany.ejercicio03_02;
 
+import Vista.Ventana1;
+import Vista.Ventana2;
 import Vista.ventana4;
 import Vista.ventanaInscripcion;
 import Vista.ventanaCrearGrupo;
@@ -14,7 +16,15 @@ import Vista.ventanaCrearGrupo;
  */
 public class Main {
     public static void main(String[] args) {
-        var v1 = new ventana4("");
+        var v1 = new Ventana1("");
         v1.show();
+         var v2 = new Ventana2("");
+        v2.show();
+         var v3 = new ventanaCrearGrupo("");
+        v3.show();
+         var v4 = new ventanaInscripcion("");
+        v4.show();
+         var v5 = new ventana4("");
+        v5.show();
     }
 }
